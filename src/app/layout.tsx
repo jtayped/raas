61ce47c28b27ab36664b1609f6bf3f0ae4d9f2d7
jtayped <jtayped@gmail.com>
2026-03-01 11:39:25 +0100

    fixed hydration warning, suppressHydrationWarning flag was in the wrong place

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,10 +59,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={inter.variable}>
+    <html lang="en" className={inter.variable} suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        suppressHydrationWarning
       >
         <RootProviders>{children}</RootProviders>
       </body>
